c9d8d76efe0cf92e13df5fb20b0c8f2c8c14b17c
a163236 <nagakeita12@gmail.com>
2024-07-27 19:55:12 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  import "carbon-components-svelte/css/g10.css";
+  import {Button, ContentSwitcher, Switch, PasswordInput, TextInput, TextArea, CodeSnippet} from "carbon-components-svelte";
+  import Copy from "carbon-icons-svelte/lib/Copy.svelte";
+</script>
+
+
+
+<h1>Key Encryptor</h1>
+
+<ContentSwitcher>
+  <Switch text="Encrypt" />
+  <Switch text="Decrypt" />
+</ContentSwitcher>
+
+
+<PasswordInput 
+  labelText="Secret Key"
+  helperText="Secret key is used to encrypt and decrypt string"
+  placeholder="Enter your secret key"
+/>
+
+<div>
+  
+</div>
+
+<PasswordInput 
+  labelText="Password"
+  helperText="The password is to be encrypted and decrypted"
+  placeholder="Enter your password to be encrypted or decrypted"
+/>
+
+<Button>
+  Encrypt
+</Button>
+
+<CodeSnippet 
+  code = "print encrypted string"
+/>
